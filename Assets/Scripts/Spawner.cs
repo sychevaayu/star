49aa69a579485e62a6f7e15059e911514c0f752d
sychevaayu <bupuppies@yandex.ru>
2022-01-10 21:31:23 +0300

    Прохождение сквозь стены

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject Snowman;
+    public float spawn_time = 3;
+    public float t = 0;
+    public int count = 5;
+
+    void Update()
+    {
+        t = t + Time.deltaTime;
+        if ((t >= spawn_time) && (count > 0))
+        {
+            Vector3 transformPosition = transform.position;
+            Vector3 spawnPoint = new Vector3(transformPosition.x + 3, transformPosition.y-1, transformPosition.z);
+            GameObject snowman = Instantiate(Snowman, spawnPoint, Quaternion.identity);
+            Destroy(snowman, 1.5f);
+            count--;
+            t = 0;
+        }
+    }
+}
